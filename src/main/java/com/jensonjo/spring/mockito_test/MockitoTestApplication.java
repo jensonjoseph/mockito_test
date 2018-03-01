ad480a0d1f313f52c4ade837e82a28ece5023b66
Kakkattil, Jenson Jos <jenson.kakkattil@ge.com>
2018-03-01 16:41:30 -0500

    Initial commit

---
+++ src/main/java/com/jensonjo/spring/mockito_test/MockitoTestApplication.java
@@ -0,0 +1,12 @@
+package com.jensonjo.spring.mockito_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jensonjo/spring/mockito_test/MockitoTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jensonjo.spring.mockito_test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockitoTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
